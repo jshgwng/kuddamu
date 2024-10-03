e8bb7b7ca708e0beda070063e3b3c232ee8aad35
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-10-03 18:26:01 +0300

    ADD: Add application configuration and implement User Details in User class

--- src/main/java/com/joshuaogwang/kuddamu/user/User.java
+++ src/main/java/com/joshuaogwang/kuddamu/user/User.java
@@ -1,11 +1,24 @@
 package com.joshuaogwang.kuddamu.user;
 
 import com.joshuaogwang.kuddamu.audit.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User extends BaseEntity {
+import java.util.Collection;
+import java.util.List;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -17,5 +30,36 @@ public class User extends BaseEntity {
     private String phoneNumber;
     @Column(nullable = true)
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role Role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(getRole().name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
