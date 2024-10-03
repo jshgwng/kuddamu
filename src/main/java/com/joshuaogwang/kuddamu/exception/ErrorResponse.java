076010ac2f367c89f7014967f12bfe2af7a772b3
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-10-03 18:13:10 +0300

    ADD: Global Application error handler

--- src/main/java/com/joshuaogwang/kuddamu/exception/ErrorResponse.java
+++ src/main/java/com/joshuaogwang/kuddamu/exception/ErrorResponse.java
@@ -1,2 +1,17 @@
-package com.joshuaogwang.kuddamu.exception;public class ErrorResponse {
+package com.joshuaogwang.kuddamu.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String title;
+    private HttpStatus status;
+    private String message;
 }
--- src/main/java/com/joshuaogwang/kuddamu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/joshuaogwang/kuddamu/exception/GlobalExceptionHandler.java
@@ -1,2 +1,15 @@
-package com.joshuaogwang.kuddamu.exception;public class GlobalExceptionHandler {
+package com.joshuaogwang.kuddamu.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e){
+        ErrorResponse errorResponse = ErrorResponse.builder().title("Error").status(HttpStatus.INTERNAL_SERVER_ERROR).message(e.getMessage()).build();
+        return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
