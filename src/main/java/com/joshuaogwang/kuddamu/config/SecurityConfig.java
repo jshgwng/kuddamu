01ee8601208e310e626442ac91e961c857efee05
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-10-03 19:12:19 +0300

    ADD: Security configuration

---
+++ src/main/java/com/joshuaogwang/kuddamu/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.joshuaogwang.kuddamu.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+    private final AuthenticationProvider provider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
+        security.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.requestMatchers("/api/v1/users/**","/v3/api-docs/**","/swagger-ui/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(provider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+    }
+}
