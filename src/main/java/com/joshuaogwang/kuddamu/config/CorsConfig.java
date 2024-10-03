02d9b7d0a0005b519cb0b381deaea836b285ea3a
Joshua Ogwang <joshua.ogwang@hotmail.com>
2024-10-03 18:29:07 +0300

    ADD: Cors filter for application endpoint

---
+++ src/main/java/com/joshuaogwang/kuddamu/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.joshuaogwang.kuddamu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter filter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/api/**", configuration);
+        return new CorsFilter(source);
+    }
+}
